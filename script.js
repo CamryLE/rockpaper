00c6019557fe615a432a9f6824b1e625002ad97d
Suze <baconbrawler123@gmail.com>
2022-09-09 09:27:23 -0400

    add initial RPS functions

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-let computerChoice;
-let playerChoice;
+const choices = ["rock", "paper", "scissors"]
 
-
-
-function getComputerChoice ()
-{
-  switch(Math.floor(Math.random() *3)) {
-    case 0:
-        computerChoice = "Rock";
-    case 1 :
-        computerChoice = "Paper"
-    case 2 :
-        computerChoice = "Scissors"
-  }
-
-  
+function game() {
+   playRound();
 }
 
-getComputerChoice();
+function playRound() {
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection, computerSelection);
+    console.log(winner)
+}
+
+
+function playerChoice () {
+    let input = prompt("Type Rock, paper, or scissors")
+    while(input == null) {
+        input = prompt("Type Rock, paper, or scissors")
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input)
+    if(check == true) {
+        console.log(input)
+    }
+    if(check == false) {
+        prompt ('You must type rock paper or scissors dummy')
+    }
+    //console.log(input)
+}
+
+function computerChoice () {
+    return choices[Math.floor(Math.random()*3)]
+}
+
+function validateInput(choice){
+    if(choices.includes(choice)) {
+        return true;
+    } else {
+        return false;
+    }
+ }
+
+function checkWinner(choiceP, choiceC) {
+    
+}
+
+
+game();
